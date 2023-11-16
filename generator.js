6e3250c2451a4f644d7e688e01a4509ec5a24a78
raulbana <99099003+raulbana@users.noreply.github.com>
2023-11-16 19:03:15 -0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,11 @@
-var letras = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'j', 'J', 'k', 'K', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '!', "@", '#', '$', '%', '&', '*', '(', ')', '?', '[', ']', 'â', 'ã', 'Â', 'Ã', 'ç', 'Ç', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-var codigo = letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)];
+const letras = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'j', 'J', 'k', 'K', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '!', "@", '#', '$', '%', '&', '*', '(', ')', '?', '[', ']', 'â', 'ã', 'Â', 'Ã', 'ç', 'Ç', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-console.log(codigo);
+function generateCode(size) {
+    let code = '';
+    for (let i = 0; i < size; i++) {
+        code += letras[parseInt(Math.random() * letras.length)];
+    }
+    return code;
+}
+
+console.log(generateCode(8))
