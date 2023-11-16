3de3047039b4c0e97fcc7363521c17c92423df95
Matheus-Grego <matheusgrego6@gmail.com>
2023-11-15 22:04:00 -0300

    [fix] tamanho dos quadrados [add] gerador de código

---
+++ generator.js
@@ -0,0 +1,4 @@
+var letras = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'j', 'J', 'k', 'K', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '!', "@", '#', '$', '%', '&', '*', '(', ')', '?', '[', ']', 'â', 'ã', 'Â', 'Ã', 'ç', 'Ç', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var codigo = letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)] + letras[parseInt(Math.random() * 78)];
+
+console.log(codigo);
