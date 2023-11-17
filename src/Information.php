086317ada063b714c355b98077ab94aba0c26d9b
raul.bana <raul.ferreira@gmail.com>
2023-11-17 09:34:06 -0300

    feat: fetch info by id

--- src/Information.php
+++ src/Information.php
@@ -1,7 +1,12 @@
 <?php 
     class Information{
-        function getUsers($conn){
-            $sql = "SELECT * FROM users";
+        function getUsers($conn, $id){
+            if($id){
+                $sql = "SELECT * FROM users WHERE idUser = ".$id."";
+
+            }else{
+                $sql = "SELECT * FROM users";
+            }
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
@@ -10,8 +15,14 @@
                 return [];
             }
         }
-        function getTeams($conn){
-            $sql = "SELECT * FROM equipes";
+        function getTeams($conn, $id){
+            if($id){
+                $sql = "SELECT * FROM equipes WHERE idEquipe = ".$id."";
+
+            }else{
+                $sql = "SELECT * FROM equipes";
+            }
+          
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
@@ -20,8 +31,15 @@
                 return [];
             }
         }
-        function getTeamsPoints($conn){
-            $sql = "SELECT B.*, SUM(points) AS PTS FROM users A INNER JOIN equipes B on A.equipe = B.idEquipe GROUP BY A.equipe ORDER BY PTS DESC";
+        function getTeamsPoints($conn, $id){
+            if($id){
+                $sql = "SELECT B.*, SUM(points) AS PTS FROM users A INNER JOIN equipes B on A.equipe = B.idEquipe WHERE B.idEquipe = ".$id." GROUP BY A.equipe ORDER BY PTS DESC";
+
+            }else{
+                $sql = "SELECT B.*, SUM(points) AS PTS FROM users A INNER JOIN equipes B on A.equipe = B.idEquipe GROUP BY A.equipe ORDER BY PTS DESC";
+            }
+            
+
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
