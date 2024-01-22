32af7976dacd6e126d616c078467cb4ba402dcf1
Gabriel Troni <troni.gabriel.10@gmail.com>
2024-01-22 13:20:21 -0300

    debug routes

--- api/src/navbar.php
+++ api/src/navbar.php
@@ -9,16 +9,16 @@
         <a href="/api/leaderboard.php"> Leaderboard </a>
         <a href="/api/equipe.php"> Equipes </a>
         <a href="/api/index.php"> Jogar </a>
-        <a href="/api/src/logout.php"> Sair </a>
         <?php
         require __DIR__ . '/Information.php';
         require __DIR__ . '/conn.php';
-
+        
         if (empty($_SESSION['cc_user'])) {
             //echo "<script>window.location.href= ' . __DIR__ . '/../login.php'</script>"
             echo "<a href='/api/login.php'> Entrar </a>";
             echo "<a href='/api/register.php'> Cadastrar-se </a>";
         } else {
+            echo "<a href='/api/src/logout.php'> Sair </a>";
             $userInfo = new Information;
             $resultUser = $userInfo->getUsers($conn,$_SESSION['cc_user']);
             echo "<p class='user-welcome'>Bem-vindo " . $resultUser['nomeUser'] . "</p>";
